f82ec2835016bcb183308eb40e720343594de7c3
Frankllin <frankllin15@hotmail.com>
2021-06-02 02:22:00 -0300

    antes da merda

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,76 @@
-import '../styles/globals.css'
+import NextApp from 'next/app'
+import React, { useContext } from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Header from "../components/Header";
+
+import SearchBar from '../components/SearchBar'
+import styled from "styled-components";
+import { AppWrapper, useAppContext, AppContext } from '../context/state';
+import { useTheme, withTheme } from '@material-ui/core/styles';
+
+const GlobalStyle = createGlobalStyle` 
+* {
+  box-sizing: border-box;
 }
 
-export default MyApp
+body {
+  background-image: src(${props => props.bgImage});
+  background-color: #161f2b;
+  max-width: 100vw;
+  padding: 0; 
+  margin: 0;
+  font-family:Verdana;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: #c4c4c4;
+  /* align-items: center; */
+}
+
+`;
+
+
+const Container = styled.div`
+  display: flex;
+  max-width: 100vw;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+`
+
+const theme = {
+
+}
+
+
+export default class App extends NextApp {
+  // remove it here
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode)
+      jssStyles.parentNode.removeChild(jssStyles)
+      
+
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+
+                   
+            <Header />
+            <SearchBar />
+          <AppWrapper>
+      <GlobalStyle theme={theme}/>
+        {/* <ThemeProvider > */}
+            <Component {...pageProps} />
+        {/* </ThemeProvider> */}
+          </AppWrapper>
+      </>
+    )
+  }
+}
+
+
