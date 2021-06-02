8b4ce86f93ca2655438cc5afeedeca9c2c3b993b
Frankllin <frankllin15@hotmail.com>
2021-06-02 02:37:04 -0300

    merda rolando

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { ThemeStore } from '../context/ThemeStore'
 import '../styles/globals.css'
+import Theme from '../Theme'
+import Header from '../components/Header'
+import SearchBar from '../components/SearchBar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+     {/* <ThemeStore> */}
+      {/* <Theme> */}
+        <Header />
+        <SearchBar />
+        <Component {...pageProps} />
+      {/* </Theme> */}
+    {/* </ThemeStore> */}
+    </>
+  )
 }
 
 export default MyApp
