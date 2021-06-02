1f50beaae8eee11cd6b8e965928f86f631e06532
Frankllin <frankllin15@hotmail.com>
2021-06-02 02:44:09 -0300

    merda rolando2

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,44 @@
-import { ThemeStore } from '../context/ThemeStore'
-import '../styles/globals.css'
-import Theme from '../Theme'
-import Header from '../components/Header'
+import NextApp from 'next/app'
+import React, { useContext } from 'react'
+
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Header from "../components/Header";
+
 import SearchBar from '../components/SearchBar'
+import styled from "styled-components";
+import { AppWrapper, useAppContext, AppContext } from '../context/state';
+import { useTheme, withTheme } from '@material-ui/core/styles';
+import { ThemeStore } from '../context/ThemeStore';
+import Theme from '../Theme';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-     {/* <ThemeStore> */}
-      {/* <Theme> */}
-        <Header />
-        <SearchBar />
-        <Component {...pageProps} />
-      {/* </Theme> */}
-    {/* </ThemeStore> */}
-    </>
-  )
+
+
+
+export default class App extends NextApp {
+  // remove it here
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode)
+      jssStyles.parentNode.removeChild(jssStyles)
+
+
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+
+        <ThemeStore>
+          <Theme>
+
+            <Header />
+            <SearchBar />
+            <Component {...pageProps} />
+          </Theme>
+        </ThemeStore>
+
+      </>
+    )
+  }
 }
-
-export default MyApp
