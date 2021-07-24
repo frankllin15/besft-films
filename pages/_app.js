46555f50daadc9a0bd9dc5296caba85ea852378d
frankllin15 <65142775+frankllin15@users.noreply.github.com>
2021-07-24 16:09:58 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,9 @@ export default class App extends NextApp {
               <script async src="https://www.googletagmanager.com/gtag/js?id=G-KMYFP1JYED"></script>
               <script>
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
+                function gtag(){
+                  dataLayer.push(arguments)
+                }
                 gtag('js', new Date());
 
                 gtag('config', 'G-KMYFP1JYED');
