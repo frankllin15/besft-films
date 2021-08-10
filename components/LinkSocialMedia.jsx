21de0ea9045971f597a15c2ecad0e123eea49fc9
Frankllin <frankllin15@hotmail.com>
2021-08-09 22:10:34 -0300

    Atualização LinkSocialMedia

--- components/LinkSocialMedia.jsx
+++ components/LinkSocialMedia.jsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 
 
 export default function LinkSosialMedia({ logo, children, bgColor, href, color }) {
+
+    bgColor = "bg-"+bgColor
     return (
-        <a color={color} href={href} className={`flex items-center justify-around mr-4 mb-4 bg-${bgColor} hover:opacity-90 min-w-93px h-10 rounded-lg p-4 `}>
+        <a color={color} href={href} className={`flex items-center justify-around mr-4 mb-4 ${bgColor} hover:opacity-90 min-w-93px h-10 rounded-lg p-4 `}>
             <Image width="32" height="32" src={logo}/>
             <p className={`inline-block ml-3 text-${color || "white"} font-bold`}>
             {children}
