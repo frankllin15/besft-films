0a94f3365500ffe28f2342e8c8edd821ec1a1167
Frankllin <frankllin15@hotmail.com>
2021-08-18 20:05:38 -0300

    Remoção de tag Image/next

--- components/LinkSocialMedia.jsx
+++ components/LinkSocialMedia.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 
 
 export default function LinkSosialMedia({ logo, children, bgColor, href, color }) {
@@ -7,7 +6,7 @@ export default function LinkSosialMedia({ logo, children, bgColor, href, color }
     bgColor = "bg-"+bgColor
     return (
         <a color={color} href={href} className={`flex items-center justify-around mr-4 mb-4 ${bgColor} hover:opacity-90 min-w-93px h-10 rounded-lg p-4 `}>
-            <Image width="32" height="32" src={logo}/>
+            <img width="32" height="32" src={logo}/>
             <p className={`inline-block ml-3 text-${color || "white"} font-bold`}>
             {children}
             </p>
